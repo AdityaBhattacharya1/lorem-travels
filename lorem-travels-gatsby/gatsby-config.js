8aa96e9bd4f9fdf0a5f6abcddc501da22a9bfc19
AdityaBhattacharya1 <69953590+AdityaBhattacharya1@users.noreply.github.com>
2021-04-27 12:27:25 +0530

    remove gatsby-browser.js

--- lorem-travels-gatsby/gatsby-config.js
+++ lorem-travels-gatsby/gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-offline`,
       options: {
-        precachePages: [`/`, `/pricing/`],
+        precachePages: [`/*`],
         workboxConfig: {
           importWorkboxFrom: `cdn`,
         },
