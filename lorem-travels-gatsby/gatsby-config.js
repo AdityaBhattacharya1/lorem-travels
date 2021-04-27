bdf95a8d1e885647aa28475e46ab98743a08d357
AdityaBhattacharya1 <69953590+AdityaBhattacharya1@users.noreply.github.com>
2021-04-27 11:09:02 +0530

    update package.json and other relevant files to suit the project details

--- lorem-travels-gatsby/gatsby-config.js
+++ lorem-travels-gatsby/gatsby-config.js
@@ -15,13 +15,26 @@ module.exports = {
     `styled-components`,
     `babel-plugin-styled-components`,
     `gatsby-plugin-transition-link`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Lorem Travels",
+        short_name: "Lorem Travels",
+        start_url: "/",
+        background_color: "#FEF5DD",
+        theme_color: "#FEF5DD",
+        display: "standalone",
+        icon: "src/images/icon.png",
+        crossOrigin: `use-credentials`,
+      },
+    },
     {
       resolve: `gatsby-plugin-offline`,
       options: {
         precachePages: [`/`, `/pricing/`],
       },
     },
-    `gatsby-plugin-react-helmet`,
   ],
   siteMetadata: {
     title: "Lorem Travels",
