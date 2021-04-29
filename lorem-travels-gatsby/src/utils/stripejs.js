dd4db460a2f2808f9e59649c0ef5502c6df8a835
AdityaBhattacharya1 <69953590+AdityaBhattacharya1@users.noreply.github.com>
2021-04-29 16:30:25 +0530

    asdf

--- lorem-travels-gatsby/src/utils/stripejs.js
+++ lorem-travels-gatsby/src/utils/stripejs.js
@@ -3,7 +3,9 @@ import { loadStripe } from "@stripe/stripe-js"
 let stripePromise
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(`process.env.SHARED_PUBLIC_API_KEY`)
+    stripePromise = loadStripe(
+      "pk_test_51IlBKHSIQ5tys5IhoScOGrH9q4lN3TaYHDzrAMis9wGOdz9Jq673bmVEaDT4gTpK1kAKyTsaG2zOrs0m6X4mN605003N49hEzC"
+    )
   }
   return stripePromise
 }
