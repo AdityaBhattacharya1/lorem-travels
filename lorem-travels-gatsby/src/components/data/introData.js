024dc4a707819fdb0a2652ec836268d32eb6f7cc
AdityaBhattacharya1 <69953590+AdityaBhattacharya1@users.noreply.github.com>
2021-05-06 11:43:02 +0530

    improve order in introData.js

--- lorem-travels-gatsby/src/components/data/introData.js
+++ lorem-travels-gatsby/src/components/data/introData.js
@@ -1,3 +1,9 @@
+export const headerData = {
+  title: "Welcome to Lorem Travels",
+  desc:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis veniam nulla autem sit, in necessitatibus reiciendis nostrum doloribus ipsa iste?",
+}
+
 export const introData = [
   {
     id: 1,
@@ -18,9 +24,3 @@ export const introData = [
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis veniam nulla autem sit, in necessitatibus reiciendis nostrum doloribus ipsa iste?",
   },
 ]
-
-export const headerData = {
-  title: "Welcome to Lorem Travels",
-  desc:
-    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis veniam nulla autem sit, in necessitatibus reiciendis nostrum doloribus ipsa iste?",
-}
